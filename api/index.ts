b25a8a8d4893cd323888f205e253088aae9f5a86
HARSH SINGH <kushwahaaashik001@gmail.com>
2026-01-31 16:55:47 +0530

    Set up Express server with routes and CORS

--- api/index.ts
+++ api/index.ts
@@ -1,1 +1,18 @@
+import express from 'express';
+import cors from 'cors';
+import sniperRoutes from '../routes/sniper';
+import leadRoutes from '../routes/leads';
 
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use('/api/sniper', sniperRoutes);
+app.use('/api/leads', leadRoutes);
+
+app.get('/', (req, res) => {
+  res.send('Optima Engine is Running... 🚀');
+});
+
+export default app;
